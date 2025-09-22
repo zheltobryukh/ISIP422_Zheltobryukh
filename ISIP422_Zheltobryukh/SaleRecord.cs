16503bc1ebb973fef42cf023a997454da9def928
227196 <227196@edu.fa.ru>
2025-09-22 14:15:46 +0300

    добавлен класс SaleRecord для учёта продаж

---
+++ ISIP422_Zheltobryukh/SaleRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP422_Zheltobryukh
+{
+    namespace ISIP422_Zheltobryukh
+    {
+        public class SaleRecord
+        {
+            public Product Product { get; }
+            public int Quantity { get; }
+            public decimal TotalPrice => Product.Price * Quantity;
+
+            public SaleRecord(Product product, int quantity)
+            {
+                Product = product;
+                Quantity = quantity;
+            }
+
+            public override string ToString()
+            {
+                return $"{Product.Name} - {Quantity} шт. на сумму {TotalPrice}₽";
+            }
+        }
+    }
+
+}
