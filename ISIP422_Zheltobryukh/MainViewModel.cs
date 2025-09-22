fdd18a0756aadae188e60b056010d4374d81a4c5
227196 <227196@edu.fa.ru>
2025-09-22 14:03:49 +0300

    Добавлен MainViewModel с тестовыми данными

---
+++ ISIP422_Zheltobryukh/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP422_Zheltobryukh
+{
+    public class MainViewModel
+    {
+        private readonly ProductService _productService;
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        public MainViewModel()
+        {
+            _productService = new ProductService();
+            _productService.SeedTestData();
+            Products = new ObservableCollection<Product>(_productService.GetAll());
+        }
+    }
+}
